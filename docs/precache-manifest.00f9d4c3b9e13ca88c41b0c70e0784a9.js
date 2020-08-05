33af72dcae168075fa2360cae4355af0a94827bb
jomiva <jomiva5016@gmail.com>
2020-08-05 07:51:05 -0400

    creada carpeta docs para deploy

---
+++ docs/precache-manifest.00f9d4c3b9e13ca88c41b0c70e0784a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4ee16907408fd6ae57e918a609afe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e2c7e4476c61c28c18",
+    "url": "/static/js/2.bf48262b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf48262b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb4b3b630d5b96835bc",
+    "url": "/static/js/main.b25f9d3c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
