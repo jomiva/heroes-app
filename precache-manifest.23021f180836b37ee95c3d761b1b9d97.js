0d5c2a0ec78d342992955485444d69a263c074ee
jomiva <jomiva5016@gmail.com>
2020-08-05 08:02:30 -0400

    Updates

---
+++ precache-manifest.23021f180836b37ee95c3d761b1b9d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf969121d129a19bfa8b9911a5f42dc",
+    "url": "/heroes-app/index.html"
+  },
+  {
+    "revision": "22e2c7e4476c61c28c18",
+    "url": "/heroes-app/static/js/2.bf48262b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/heroes-app/static/js/2.bf48262b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb4b3b630d5b96835bc",
+    "url": "/heroes-app/static/js/main.b25f9d3c.chunk.js"
+  },
+  {
+    "revision": "ac13283cadb28abfd87b",
+    "url": "/heroes-app/static/js/runtime-main.dca4f873.js"
+  }
+]);
